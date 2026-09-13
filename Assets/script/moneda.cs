a2b7b8759375aa53389938e1cdf684d6d75b2397
NiloUelCuroVila <76021246@continental.edu.pe>
2026-09-13 00:27:34 -0500

    mejora UX

--- Assets/script/moneda.cs
+++ Assets/script/moneda.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class moneda : MonoBehaviour
 {
+    [SerializeField] private AudioClip sonidoRecoger;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             contadorMonedas uiManager = Object.FindObjectOfType<contadorMonedas>();
             uiManager.CoinColleted();
-            Destroy(gameObject); // Destruye la moneda despu�s de ser recogida
+
+            if (sonidoRecoger != null)
+            {
+                // PlayClipAtPoint crea su propio AudioSource temporal, asi el sonido
+                // no se corta al destruir la moneda en este mismo frame
+                AudioSource.PlayClipAtPoint(sonidoRecoger, transform.position);
+            }
+
+            Destroy(gameObject); // Destruye la moneda despues de ser recogida
         }
     }
 }
