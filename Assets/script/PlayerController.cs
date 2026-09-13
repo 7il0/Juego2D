4a91136163f9aa3e95805629faa624018df37e15
NiloUelCuroVila <76021246@continental.edu.pe>
2026-09-13 01:59:13 -0500

    nuevos sonidos

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -15,10 +15,22 @@ public class PlayerController : MonoBehaviour
 
     public Animator animator;
 
+    // Un sonido en loop por cada estado (igual que las 4 animaciones: respirar, caminar, saltar, caida)
+    [SerializeField] private AudioClip sonidoEstatico;
+    [SerializeField] private AudioClip sonidoAvanzar;
+    [SerializeField] private AudioClip sonidoSaltar;
+    [SerializeField] private AudioClip sonidoCaer;
+
+    private AudioSource audioEstado;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        audioEstado = gameObject.AddComponent<AudioSource>();
+        audioEstado.loop = true;
+        audioEstado.playOnAwake = false;
 
         // Punto de reaparicion por defecto si el jugador aun no pisa ninguna bandera
         GameManager.Instance.RegistrarSpawnInicial(transform.position);
@@ -43,10 +55,47 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("velocidad", Mathf.Abs(move));
         animator.SetFloat("velocidad_vertical", rb2D.linearVelocity.y);
         animator.SetBool("saltando", isGrounded);
+
+        ActualizarSonidoDeEstado();
     }
 
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+    }
+
+    /// <summary>
+    /// Elige que clip debe sonar segun el mismo estado que ya usa el Animator,
+    /// y solo reinicia el AudioSource cuando el clip realmente cambia.
+    /// </summary>
+    private void ActualizarSonidoDeEstado()
+    {
+        AudioClip clipDeseado;
+
+        if (!isGrounded)
+        {
+            // En el aire: subiendo es "saltar", bajando es "caida"
+            clipDeseado = rb2D.linearVelocity.y > 0f ? sonidoSaltar : sonidoCaer;
+        }
+        else
+        {
+            clipDeseado = move != 0f ? sonidoAvanzar : sonidoEstatico;
+        }
+
+        if (audioEstado.clip == clipDeseado)
+        {
+            return;
+        }
+
+        audioEstado.clip = clipDeseado;
+
+        if (clipDeseado != null)
+        {
+            audioEstado.Play();
+        }
+        else
+        {
+            audioEstado.Stop();
+        }
     }
 }
