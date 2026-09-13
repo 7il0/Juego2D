78c7aa41595e610157b0dba16bd54289c476502c
NiloUelCuroVila <76021246@continental.edu.pe>
2026-09-13 01:05:38 -0500

    nuevo sonido

--- Assets/script/puas.cs
+++ Assets/script/puas.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class puas : MonoBehaviour
 {
+    [SerializeField] private AudioClip sonidoGolpe;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
+            if (sonidoGolpe != null)
+            {
+                // PlayClipAtPoint no depende de Time.timeScale, asi que suena
+                // aunque el juego se congele justo despues por el panel de Game Over
+                AudioSource.PlayClipAtPoint(sonidoGolpe, transform.position);
+            }
+
             GameManager.Instance.Morir();
         }
     }
